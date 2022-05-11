cf57d018d310fb661de258827d036e0e89515484
hoangkimsgu <hoangkim.sgu@gmail.com>
2022-05-11 21:32:08 +0700

    update web speech api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const API = 'daa0212fbd13a05d0ba5bc59cfd50f8a'
 
 // components that need to be rendered
+const app = $('.app')
 const inputCity = $('.app_header-search-input')
 const DEFAULT_VALUE = '--'
 const position = $('.app_main-position')
@@ -41,15 +42,15 @@ const renderContent = (data) => {
 
 }
 
-inputCity.change((e) => {
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${API}&lang=vi&units=metric`)
+//handle data and show location on map
+const handleData = (position) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${position}&appid=${API}&lang=vi&units=metric`)
         .then(async res => {
             const data = await res.json()
             console.log(data);
             //reset input
-            e.target.value = ''
-            inputCity.focus()
+
 
             if (data.cod == 404) {
 
@@ -69,7 +70,7 @@ inputCity.change((e) => {
                 container._leaflet_id = null;
             }
 
-            //
+            //  
             var accessToken = 'pk.eyJ1IjoiaGtpbTU4OCIsImEiOiJjbDMwN2ZldnMwMHVlM2NvMm1kcWw3am10In0.L0Tywl3fahbhNvK6MY3EDw'
 
             var map = L.map('map').setView([latMap, lonMap], 11);
@@ -93,9 +94,104 @@ inputCity.change((e) => {
             return
         })
 
+}
+
+inputCity.change((e) => {
+
+    handleData(e.target.value)
+
+    e.target.value = ''
+    inputCity.focus()
 })
 
-// virtual assistance
-microphone.click(() => {
-    console.log('listning... ')
+///////////////////////////
+// virtual assistance 
+
+const microphone2 = document.querySelector('.microphone')
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+const requestBackground = ['sáng', 'tối', 'mặc định']
+const backgroundColor = ['#fff', '#333', '#3498DB']
+const hour = new Date().getHours()
+const minute = new Date().getMinutes()
+
+console.log(hour, minute);
+
+let requestStr = ''
+
+// get position from voice message
+const getPosition = (str) => {
+    const arr = str.split('tại')
+    const result = arr[1].trim()
+    console.log(result);
+    return result
+}
+
+const getColor = (str) => {
+    const arr = str.split('màu nền')
+    const request = arr[1].trim()
+    const index = requestBackground.indexOf(request)
+    return backgroundColor[index]
+}
+
+// set config 
+recognition.lang = 'vi-VI';
+recognition.continuous = false;
+
+
+microphone2.addEventListener('click', (e) => {
+    e.preventDefault
+    recognition.start();
+    microphone.addClass('microphone--listening')
+    console.log('Listening...');
+
 })
+
+recognition.onspeechend = function () {
+    recognition.stop();
+    microphone.removeClass('microphone--listening')
+    console.log('end...');
+}
+
+recognition.onerror = (err) => {
+    console.error(err);
+}
+
+
+
+recognition.onresult = (e) => {
+
+    requestStr = e.results[0][0].transcript
+
+    var isRequestWeatherForecast = requestStr.includes('thời tiết')
+    var isRequestBackgroundColor = requestStr.includes('màu nền')
+    var isRequestBackgroundColor = requestStr.includes('mấy giờ')
+    console.log(requestStr);
+
+    //
+    if (isRequestWeatherForecast) {
+        const position = getPosition(requestStr)
+        handleData(position)
+    }
+
+    //
+    if (isRequestBackgroundColor) {
+        const color = getColor(requestStr)
+
+        if (color == '#fff') {
+            app.attr('style', `background-color: ${color}; color: #333 `)
+        } else {
+            app.attr('style', `background-color: ${color}; color: #fff `)
+        }
+        console.log(color);
+    }
+
+    //
+
+
+}
+
+
+// login facebook
+
+
